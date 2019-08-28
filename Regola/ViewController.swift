0cc49459459755630c3a6fd0d749a6afe17edb03
Xu Jiawei <jiawei.xu@centrixlink.com>
2019-08-28 14:51:42 +0800

    Initial Commit

---
+++ Regola/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Regola
+//
+//  Created by yysz02 on 2019/8/28.
+//  Copyright © 2019 farteen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
